f13ffa32cbe6175e5e2edd695b52150d9e5eb7cd
ArturAzarau <artur.azarau@gmail.com>
2018-08-08 22:29:32 +0300

    - mapView created - Capital class created

---
+++ Capital cities/Capital.swift
@@ -0,0 +1,27 @@
+//
+//  Capital.swift
+//  Capital cities
+//
+//  Created by Артур Азаров on 08.08.2018.
+//  Copyright © 2018 Артур Азаров. All rights reserved.
+//
+
+import Foundation
+import MapKit
+
+final class Capital: NSObject, MKAnnotation {
+    
+    // MARK: - Properties
+    
+    var title: String?
+    var coordinate: CLLocationCoordinate2D
+    var info: String
+    
+    // MARK: - Life cycle
+    
+    init(title: String, coordinate: CLLocationCoordinate2D, info: String) {
+        self.title = title
+        self.coordinate = coordinate
+        self.info = info
+    }
+}
